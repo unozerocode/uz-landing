e536278844a79409b8ca34809c0d6e4a25f99b2e
unozerocode <enrique@unozerocode.com>
2020-05-07 17:48:05 -0500

    Added intercom

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react");
+
+
+exports.onRenderBody = function({ setPostBodyComponents }) {
+
+
+    setPostBodyComponents([
+       
+        <script dangerouslySetInnerHTML={{
+            __html: `  window.intercomSettings = {
+                app_id: "x7hacebo"
+              };`,
+        }}
+    />
+    ,
+    <script dangerouslySetInnerHTML={{
+        __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/x7hacebo';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`,
+    }}
+/>
+    ]);
+
+};
---
+++ src/pages/contacto.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header />
+    <p>Contáctanos</p>
+    <a href="https://share.hsforms.com/1MpvUV0NMSUSzLeBEmTi-tg4izye">Contacto</a>
+
+  </div>
+)
