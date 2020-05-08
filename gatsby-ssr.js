c468e6b403a3f83f4c26c1f75cd263b82192ab42
unozerocode <enrique@unozerocode.com>
2020-05-08 10:59:34 -0500

    remove intercom chatbot, add hubspot tracking code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -14,16 +14,10 @@ exports.onRenderBody = function({ setPostBodyComponents }) {
     setPostBodyComponents([
        
         <script dangerouslySetInnerHTML={{
-            __html: `  window.intercomSettings = {
-                app_id: "x7hacebo"
-              };`,
+            __html: `type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7604870.js"`,
         }}
     />
-    ,
-    <script dangerouslySetInnerHTML={{
-        __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/x7hacebo';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`,
-    }}
-/>
     ]);
 
 };
+
